1bac51d36a40ce9bfbb8af6a4b90249a6eb07822
Habu <habu1010+github@gmail.com>
2021-01-26 18:47:33 +0900

    [feature] ダイスロールの結果をEmbedで返信する
    ダイスロールの結果を従来のプレインテキストでの通知から、Embedでの通知に変更する。
    title に合計、description に各投の結果を表示する。

--- DiceRoll.py
+++ DiceRoll.py
@@ -1,6 +1,7 @@
 import random
 import re
 
+import discord
 from discord.ext import commands
 
 
@@ -19,15 +20,15 @@ class DiceRoll(commands.Cog):
         side = int(m.group(2))
         if dice > 0 and side > 0:
             result_msg = self.__roll(dice, side)
-            await ctx.reply(result_msg)
+            await ctx.reply(embed=result_msg)
 
-    def __roll(self, dice: int, side: int):
+    def __roll(self, dice: int, side: int) -> discord.Embed:
         if dice > 100:
-            return '振る回数が多すぎます'
+            return discord.Embed(title='振る回数が多すぎます', color=discord.Color.red())
         roll_results = [self.rng.randint(1, side) for _ in range(dice)]
         roll_sum = sum(roll_results)
-        result_seq = ','.join(str(i) for i in roll_results)
-        return "{seq} = {sum}".format(seq=result_seq, sum=roll_sum)
+        result_seq = '[' + ','.join(str(i) for i in roll_results) + ']'
+        return discord.Embed(title=roll_sum, description=result_seq)
 
 
 def setup(bot):
