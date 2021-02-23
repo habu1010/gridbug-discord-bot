78e5384a2c54bbc11eb4c2f48d8a65e6c66b5df8
Habu <habu1010+github@gmail.com>
2021-02-23 11:49:15 +0900

    [feature] 選択用のリアクション表示のキャンセル
    候補の選択用のリアクションを表示している途中に
    リアクションの選択を可能にし、選択した場合は
    続くリアクションの表示をキャンセルして選択した
    候補の表示を行うようにする。

--- CandidatesSelector.py
+++ CandidatesSelector.py
@@ -20,10 +20,12 @@ async def select(ctx: commands.Context, prompt_msg: str, candidates: List[str]):
 async def send_candidates_msg(ctx: commands.Context, prompt_msg: str, candidates: List[str]):
     candidates_text = '\n'.join([n + " " + c for n, c in zip(NUM_EMOJIS, candidates)])
     embed = discord.Embed(title=prompt_msg, description=candidates_text)
-    msg: discord.Message = await ctx.reply(embed=embed)
-    for num in NUM_EMOJIS[:len(candidates)]:
-        await msg.add_reaction(num)
-    return msg
+    return await ctx.reply(embed=embed)
+
+
+async def add_candidates_select_reactions(candidates_msg: discord.Message, candidate_num: int):
+    for num in NUM_EMOJIS[:candidate_num]:
+        await candidates_msg.add_reaction(num)
 
 
 async def wait_for_selection(ctx: commands.Context, candidates_msg: discord.Message, candidate_num: int):
@@ -32,9 +34,14 @@ async def wait_for_selection(ctx: commands.Context, candidates_msg: discord.Mess
             ctx.message.author.id == payload.user_id and \
             payload.message_id == candidates_msg.id and \
             str(payload.emoji) in NUM_EMOJIS[:candidate_num]
+
+    add_reaction_task = asyncio.create_task(add_candidates_select_reactions(candidates_msg, candidate_num))
+
     try:
         payload = await ctx.bot.wait_for('raw_reaction_add', timeout=15.0, check=check)
     except asyncio.TimeoutError:
         return None
+    finally:
+        add_reaction_task.cancel()
 
     return NUM_EMOJIS.index(str(payload.emoji))
